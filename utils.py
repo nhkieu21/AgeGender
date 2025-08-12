4d9f913fc5ad408e4ded80782441c0b1c6c37a24
Như Kiều <nhukieu21062003@gmail.com>
2025-08-12 16:42:18 +0700

    fix tí

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.485, 0.456, 0.406])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 gender_labels=['Male', 'Female']
